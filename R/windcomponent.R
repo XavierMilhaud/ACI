ccf616bd8f8571e1d190fa6cd3a03e47cbd5f7e3
dabakh <el-hadji-abdoul-aziz.kane@grenoble-inp.org>
2024-07-10 17:09:06 +0200

    WIP : working on wind component

--- R/windcomponent.R
+++ R/windcomponent.R
@@ -1,109 +1,105 @@
 library(ncdf4)
-library(data.table)
 library(lubridate)
+library(data.table)
 
 WindComponent <- setRefClass(
   "WindComponent",
   fields = list(
-    u10_path = "character",
-    v10_path = "character",
-    mask_path = "character",
-    reference_period = "character",
     u10 = "array",
     v10 = "array",
-    mask = "array",
-    time_index = "Date"
+    mask = "matrix",
+    times = "Date",
+    latitudes = "numeric",
+    longitudes = "numeric"
   ),
   methods = list(
     initialize = function(u10_path, v10_path, mask_path, reference_period) {
-      .self$u10_path <<- u10_path
-      .self$v10_path <<- v10_path
-      .self$mask_path <<- mask_path
-      .self$reference_period <<- reference_period
-      .self$load_data()
+      u10_data <- ncdf4::nc_open(u10_path)
+      v10_data <- ncdf4::nc_open(v10_path)
+      mask_data <- ncdf4::nc_open(mask_path)
+      
+      .self$u10 <- ncdf4::ncvar_get(u10_data, "u10")
+      .self$v10 <- ncdf4::ncvar_get(v10_data, "v10")
+      .self$mask <- ncdf4::ncvar_get(mask_data, "country")
+      
+      .self$times <- as.Date(ncdf4::ncvar_get(u10_data, "time"), origin = "2000-01-01")
+      .self$latitudes <- as.numeric(ncdf4::ncvar_get(u10_data, "lat"))
+      .self$longitudes <- as.numeric(ncdf4::ncvar_get(u10_data, "lon"))
+      
+      ncdf4::nc_close(u10_data)
+      ncdf4::nc_close(v10_data)
+      ncdf4::nc_close(mask_data)
     },
     
-    load_data = function() {
-      u10_nc <- ncdf4::nc_open(.self$u10_path)
-      v10_nc <- ncdf4::nc_open(.self$v10_path)
-      mask_nc <- ncdf4::nc_open(.self$mask_path)
-      
-      .self$u10 <<- ncdf4::ncvar_get(u10_nc, "u10")
-      .self$v10 <<- ncdf4::ncvar_get(v10_nc, "v10")
-      .self$mask <<- ncdf4::ncvar_get(mask_nc, "country")
-      .self$time_index <<- as.Date(ncdf4::ncvar_get(u10_nc, "time"), origin="1970-01-01")
-      
-      ncdf4::nc_close(u10_nc)
-      ncdf4::nc_close(v10_nc)
-      ncdf4::nc_close(mask_nc)
+    apply_mask = function(data) {
+      mask_expanded <- array(.self$mask, dim = dim(data))
+      masked_data <- data * mask_expanded
+      return(masked_data)
     },
     
-    wind_power = function() {
-      sqrt(.self$u10^2 + .self$v10^2)
+    wind_power = function(reference_period = NULL) {
+      u10_masked <- .self$apply_mask(.self$u10)
+      v10_masked <- .self$apply_mask(.self$v10)
+      
+      ws <- sqrt(u10_masked^2 + v10_masked^2)
+      rho <- 1.23  # Air density constant
+      
+      dailymean_ws <- apply(ws, c(2, 3), function(x) tapply(x, format(.self$times, "%Y-%m-%d"), mean))
+      wind_power <- 0.5 * rho * dailymean_ws^3
+      
+      if (!is.null(reference_period)) {
+        start_date <- as.Date(reference_period[1])
+        end_date <- as.Date(reference_period[2])
+        time_indices <- which(.self$times >= start_date & .self$times <= end_date)
+        wind_power <- wind_power[time_indices, , drop = FALSE]
+      }
+      
+      # Transform to data.table
+      wind_power_dt <- data.table::data.table(
+        time = rep(as.Date(rownames(wind_power)), each = ncol(wind_power)),
+        latitude = rep(.self$latitudes, times = nrow(wind_power)),
+        longitude = rep(.self$longitudes, each = nrow(wind_power)),
+        wind_power = as.vector(wind_power)
+      )
+      
+      return(wind_power_dt)
     },
     
     wind_thresholds = function(reference_period) {
-      start_date <- as.Date(reference_period[1])
-      end_date <- as.Date(reference_period[2])
-      
-      time_index_subset <- .self$time_index[.self$time_index >= start_date & .self$time_index <= end_date]
-      wind_power_subset <- .self$wind_power()[, , which(.self$time_index %in% time_index_subset)]
-      
-      threshold <- apply(wind_power_subset, c(2, 3), function(x) quantile(x, probs = 0.9))
-      
-      dt <- data.table(
-        time = rep(time_index_subset, each = length(.self$mask)),
-        longitude = rep(seq(2.20, 2.30, by = 0.1), each = length(.self$mask) * length(time_index_subset)),
-        latitude = rep(seq(48.80, 48.90, by = 0.1), times = length(.self$mask) * length(time_index_subset)),
-        threshold = as.vector(threshold)
-      )
-      
-      dt
+      wind_power_data <- .self$wind_power(reference_period)
+      threshold <- apply(wind_power_data, c(2, 3), function(x) quantile(x, 0.95))
+      return(threshold)
     },
     
     days_above_thresholds = function(reference_period) {
-      thresholds <- .self$wind_thresholds(reference_period)$threshold
-      wind_power <- .self$wind_power()
+      wind_power_data <- .self$wind_power(reference_period)
+      thresholds <- .self$wind_thresholds(reference_period)
       
-      days_above <- apply(wind_power, c(2, 3), function(x) sum(x > thresholds))
-      
-      dt <- data.table(
-        time = seq.Date(as.Date(reference_period[1]), as.Date(reference_period[2]), by = "day"),
-        longitude = rep(seq(2.20, 2.30, by = 0.1), each = length(days_above)),
-        latitude = rep(seq(48.80, 48.90, by = 0.1), times = length(days_above)),
-        days_above_threshold = as.vector(days_above)
-      )
-      
-      dt
+      days_above <- array(0, dim = dim(wind_power_data))
+      for (i in 1:dim(wind_power_data)[1]) {
+        for (j in 1:dim(wind_power_data)[2]) {
+          for (k in 1:dim(wind_power_data)[3]) {
+            if (wind_power_data[i, j, k] > thresholds[j, k]) {
+              days_above[i, j, k] <- 1
+            }
+          }
+        }
+      }
+      return(days_above)
     },
     
     wind_exceedance_frequency = function(reference_period) {
       days_above <- .self$days_above_thresholds(reference_period)
-      total_days <- length(seq.Date(as.Date(reference_period[1]), as.Date(reference_period[2]), by = "day"))
-      
-      dt <- days_above[, .(
-        exceedance_frequency = days_above_threshold / total_days
-      ), by = .(longitude, latitude)]
-      
-      dt
+      exceedance_frequency <- apply(days_above, c(2, 3), mean)
+      return(exceedance_frequency)
     },
     
     std_wind_exceedance_frequency = function(reference_period) {
-      exceedance_frequency <- .self$wind_exceedance_frequency(reference_period)$exceedance_frequency
-      
-      mean_freq <- mean(exceedance_frequency, na.rm = TRUE)
-      std_freq <- sd(exceedance_frequency, na.rm = TRUE)
-      
-      std_frequency <- (exceedance_frequency - mean_freq) / std_freq
-      
-      dt <- data.table(
-        time = seq.Date(as.Date(reference_period[1]), as.Date(reference_period[2]), by = "month"),
-        longitude = rep(seq(2.20, 2.30, by = 0.1), each = length(std_frequency)),
-        latitude = rep(seq(48.80, 48.90, by = 0.1), times = length(std_frequency)),
-        std_frequency = as.vector(std_frequency)
-      )
-      
-      dt
+      exceedance_frequency <- .self$wind_exceedance_frequency(reference_period)
+      mean_frequency <- mean(exceedance_frequency)
+      std_frequency <- sd(exceedance_frequency)
+      standardized_frequency <- (exceedance_frequency - mean_frequency) / std_frequency
+      return(standardized_frequency)
     }
   )
 )
