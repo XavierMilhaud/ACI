50085c6477999cef2c9f4510e60e2f0e59d8f332
dabakh <el-hadji-abdoul-aziz.kane@grenoble-inp.org>
2024-07-17 15:45:41 +0200

    fix : wind component passes all its tests

--- R/windcomponent.R
+++ R/windcomponent.R
@@ -5,101 +5,120 @@ library(data.table)
 WindComponent <- setRefClass(
   "WindComponent",
   fields = list(
-    u10 = "array",
-    v10 = "array",
-    mask = "matrix",
+    data_dt = "data.table",
     times = "Date",
     latitudes = "numeric",
     longitudes = "numeric"
   ),
   methods = list(
-    initialize = function(u10_path, v10_path, mask_path, reference_period) {
-      u10_data <- ncdf4::nc_open(u10_path)
-      v10_data <- ncdf4::nc_open(v10_path)
-      mask_data <- ncdf4::nc_open(mask_path)
-      
-      .self$u10 <- ncdf4::ncvar_get(u10_data, "u10")
-      .self$v10 <- ncdf4::ncvar_get(v10_data, "v10")
-      .self$mask <- ncdf4::ncvar_get(mask_data, "country")
-      
-      .self$times <- as.Date(ncdf4::ncvar_get(u10_data, "time"), origin = "2000-01-01")
-      .self$latitudes <- as.numeric(ncdf4::ncvar_get(u10_data, "lat"))
-      .self$longitudes <- as.numeric(ncdf4::ncvar_get(u10_data, "lon"))
-      
-      ncdf4::nc_close(u10_data)
-      ncdf4::nc_close(v10_data)
-      ncdf4::nc_close(mask_data)
-    },
-    
-    apply_mask = function(data) {
-      mask_expanded <- array(.self$mask, dim = dim(data))
-      masked_data <- data * mask_expanded
-      return(masked_data)
-    },
-    
-    wind_power = function(reference_period = NULL) {
-      u10_masked <- .self$apply_mask(.self$u10)
-      v10_masked <- .self$apply_mask(.self$v10)
-      
-      ws <- sqrt(u10_masked^2 + v10_masked^2)
-      rho <- 1.23  # Air density constant
-      
-      dailymean_ws <- apply(ws, c(2, 3), function(x) tapply(x, format(.self$times, "%Y-%m-%d"), mean))
-      wind_power <- 0.5 * rho * dailymean_ws^3
-      
-      if (!is.null(reference_period)) {
-        start_date <- as.Date(reference_period[1])
-        end_date <- as.Date(reference_period[2])
-        time_indices <- which(.self$times >= start_date & .self$times <= end_date)
-        wind_power <- wind_power[time_indices, , drop = FALSE]
+    initialize = function(u10_path, v10_path, mask_path) {
+      # Function to convert NetCDF to data.table
+      nc_to_dt <- function(nc_file, var_name, time_var = "time", lat_var = "latitude", lon_var = "longitude") {
+        nc_data <- ncdf4::nc_open(nc_file)
+        var_data <- ncvar_get(nc_data, var_name)
+        
+        if (!is.null(time_var) && time_var %in% names(nc_data$dim)) {
+          time <- as.Date(ncvar_get(nc_data, time_var), origin = "2000-01-01")
+        } else {
+          time <- NULL
+        }
+        
+        latitude <- as.numeric(ncvar_get(nc_data, lat_var))
+        longitude <- as.numeric(ncvar_get(nc_data, lon_var))
+        
+        nc_close(nc_data)
+        
+        if (!is.null(time)) {
+          dt <- data.table(
+            time = rep(time, each = length(latitude) * length(longitude)),
+            latitude = rep(rep(latitude, each = length(longitude)), times = length(time)),
+            longitude = rep(longitude, times = length(latitude) * length(time)),
+            var = as.vector(var_data)
+          )
+        } else {
+          dt <- data.table(
+            latitude = rep(latitude, each = length(longitude)),
+            longitude = rep(longitude, times = length(latitude)),
+            var = as.vector(var_data)
+          )
+        }
+        
+        return(dt)
       }
       
-      # Transform to data.table
-      wind_power_dt <- data.table::data.table(
-        time = rep(as.Date(rownames(wind_power)), each = ncol(wind_power)),
-        latitude = rep(.self$latitudes, times = nrow(wind_power)),
-        longitude = rep(.self$longitudes, each = nrow(wind_power)),
-        wind_power = as.vector(wind_power)
-      )
+      u10_dt <- nc_to_dt(u10_path, "u10", time_var = "time", lat_var = "latitude", lon_var = "longitude")
+      v10_dt <- nc_to_dt(v10_path, "v10", time_var = "time", lat_var = "latitude", lon_var = "longitude")
+      mask_dt <- nc_to_dt(mask_path, "country", time_var = NULL, lat_var = "lat", lon_var = "lon")
       
-      return(wind_power_dt)
+      setnames(u10_dt, "var", "u10")
+      setnames(v10_dt, "var", "v10")
+      setnames(mask_dt, "var", "mask")
+      
+      mask_dt <- mask_dt[, .(latitude, longitude, mask)]
+      
+      data <- merge(u10_dt, v10_dt, by = c("time", "latitude", "longitude"))
+      data <- merge(data, mask_dt, by = c("latitude", "longitude"))
+      
+      data[, u10 := u10 * mask]
+      data[, v10 := v10 * mask]
+      
+      .self$data_dt <<- data
+      .self$times <<- unique(data$time)
+      .self$latitudes <<- unique(data$latitude)
+      .self$longitudes <<- unique(data$longitude)
+    },
+    
+    wind_power = function() {
+      data_dt <- .self$data_dt
+      data_dt[, ws := sqrt(u10^2 + v10^2)]
+      
+      dailymean_ws <- data_dt[, .(dailymean_ws = mean(ws, na.rm = TRUE)), by = .(time, latitude, longitude)]
+      
+      rho <- 1.23  # Air density constant
+      dailymean_ws[, wind_power := 0.5 * rho * dailymean_ws^3]
+      
+      return(dailymean_ws)
     },
     
     wind_thresholds = function(reference_period) {
-      wind_power_data <- .self$wind_power(reference_period)
-      threshold <- apply(wind_power_data, c(2, 3), function(x) quantile(x, 0.95))
-      return(threshold)
+      wind_power_data <- .self$wind_power()
+      wind_power_data[, dayofyear := yday(time)]
+      
+      reference_data <- wind_power_data[time >= as.Date(reference_period[1]) & time <= as.Date(reference_period[2])]
+      reference_stats <- reference_data[, .(mean_wind_power = mean(wind_power, na.rm = TRUE), 
+                                            std_wind_power = sd(wind_power, na.rm = TRUE)), 
+                                        by = .(dayofyear, latitude, longitude)]
+      
+      wind_power_data <- merge(wind_power_data, reference_stats, by = c("dayofyear", "latitude", "longitude"), all.x = TRUE)
+      wind_power_data[, threshold := mean_wind_power + 1.28 * std_wind_power]
+      
+      return(wind_power_data[, .(time, latitude, longitude, threshold)])
     },
     
     days_above_thresholds = function(reference_period) {
-      wind_power_data <- .self$wind_power(reference_period)
+      wind_power_data <- .self$wind_power()
       thresholds <- .self$wind_thresholds(reference_period)
       
-      days_above <- array(0, dim = dim(wind_power_data))
-      for (i in 1:dim(wind_power_data)[1]) {
-        for (j in 1:dim(wind_power_data)[2]) {
-          for (k in 1:dim(wind_power_data)[3]) {
-            if (wind_power_data[i, j, k] > thresholds[j, k]) {
-              days_above[i, j, k] <- 1
-            }
-          }
-        }
-      }
-      return(days_above)
+      days_above <- merge(wind_power_data, thresholds, by = c("time", "latitude", "longitude"))
+      days_above[, days_above_threshold := ifelse(wind_power > threshold, 1, 0)]
+      
+      return(days_above[, .(time, latitude, longitude, days_above_threshold)])
     },
     
     wind_exceedance_frequency = function(reference_period) {
       days_above <- .self$days_above_thresholds(reference_period)
-      exceedance_frequency <- apply(days_above, c(2, 3), mean)
-      return(exceedance_frequency)
+      days_above[, year_month := floor_date(time, "month")]
+      exceedance_freq <- days_above[, .(exceedance_frequency = sum(days_above_threshold, na.rm = TRUE) / .N), 
+                                    by = .(year_month, latitude, longitude)]
+      return(exceedance_freq)
     },
     
     std_wind_exceedance_frequency = function(reference_period) {
-      exceedance_frequency <- .self$wind_exceedance_frequency(reference_period)
-      mean_frequency <- mean(exceedance_frequency)
-      std_frequency <- sd(exceedance_frequency)
-      standardized_frequency <- (exceedance_frequency - mean_frequency) / std_frequency
-      return(standardized_frequency)
+      exceedance_freq <- .self$wind_exceedance_frequency(reference_period)
+      mean_frequency <- mean(exceedance_freq$exceedance_frequency, na.rm = TRUE)
+      std_frequency <- sd(exceedance_freq$exceedance_frequency, na.rm = TRUE)
+      exceedance_freq[, std_frequency := (exceedance_frequency - mean_frequency) / std_frequency]
+      return(exceedance_freq)
     }
   )
 )
--- tests/test_wind.R
+++ tests/test_wind.R
@@ -6,11 +6,10 @@ library(ggplot2)
 
 source("../R/windcomponent.R")
 
-# Helper function to create NetCDF files for testing
 create_test_nc <- function(file_path, var_name, var_data, times, latitudes, longitudes) {
   dim_time <- ncdim_def(name = "time", units = "days since 2000-01-01", vals = as.numeric(times - as.Date("2000-01-01")))
-  dim_lat <- ncdim_def(name = "lat", units = "degrees_north", vals = latitudes)
-  dim_lon <- ncdim_def(name = "lon", units = "degrees_east", vals = longitudes)
+  dim_lat <- ncdim_def(name = "latitude", units = "degrees_north", vals = latitudes)
+  dim_lon <- ncdim_def(name = "longitude", units = "degrees_east", vals = longitudes)
   
   var_def <- ncvar_def(name = var_name, units = "", dim = list(dim_time, dim_lat, dim_lon))
   nc <- nc_create(file_path, var_def)
@@ -44,11 +43,11 @@ test_that("WindComponent: wind_power method works correctly", {
   ncvar_put(nc, "country", mask_data)
   nc_close(nc)
   
-  wind <- WindComponent$new(u10_path, v10_path, mask_path, reference_period)
+  wind <- WindComponent$new(u10_path, v10_path, mask_path)
   wind_power <- wind$wind_power()
   
-  expect_is(wind_power, "array")
-  expect_equal(dim(wind_power), c(length(times), length(latitudes), length(longitudes)))
+  expect_is(wind_power, "data.table")
+  expect_true(all(c("time", "latitude", "longitude", "wind_power") %in% colnames(wind_power)))
   
   unlink(c(u10_path, v10_path, mask_path))
 })
@@ -56,7 +55,7 @@ test_that("WindComponent: wind_power method works correctly", {
 test_that("WindComponent: wind_thresholds method works correctly", {
   mask_path <- "test_mask.nc"
   u10_path <- "test_u10.nc"
-  v10_path <- "test_v 10.nc"
+  v10_path <- "test_v10.nc"
   reference_period <- c("2000-01-01", "2009-12-31")
   
   times <- seq(as.Date('2000-01-01'), as.Date('2020-12-31'), by = "days")
@@ -79,15 +78,13 @@ test_that("WindComponent: wind_thresholds method works correctly", {
   ncvar_put(nc, "country", mask_data)
   nc_close(nc)
   
-  wind <- WindComponent$new(u10_path, v10_path, mask_path, reference_period)
+  wind <- WindComponent$new(u10_path, v10_path, mask_path)
   wind_thresholds <- wind$wind_thresholds(reference_period)
   
-  print(wind_thresholds)  # Impression pour le débogage
-  
   expect_is(wind_thresholds, "data.table")
-  expect_true(all(c("longitude", "latitude", "threshold") %in% colnames(wind_thresholds)))
+  expect_true(all(c("time", "latitude", "longitude", "threshold") %in% colnames(wind_thresholds)))
   
-  unlink(c(u10_path  , v10_path, mask_path))
+  unlink(c(u10_path, v10_path, mask_path))
 })
 
 test_that("WindComponent: days_above_thresholds method works correctly", {
@@ -116,13 +113,11 @@ test_that("WindComponent: days_above_thresholds method works correctly", {
   ncvar_put(nc, "country", mask_data)
   nc_close(nc)
   
-  wind <- WindComponent$new(u10_path, v10_path, mask_path, reference_period)
+  wind <- WindComponent$new(u10_path, v10_path, mask_path)
   days_above_thresholds <- wind$days_above_thresholds(reference_period)
   
-  print(days_above_thresholds)  # Impression pour le débogage
-  
   expect_is(days_above_thresholds, "data.table")
-  expect_true(all(c("longitude", "latitude", "days_above_threshold") %in% colnames(days_above_thresholds)))
+  expect_true(all(c("time", "latitude", "longitude", "days_above_threshold") %in% colnames(days_above_thresholds)))
   
   unlink(c(u10_path, v10_path, mask_path))
 })
@@ -153,13 +148,11 @@ test_that("WindComponent: wind_exceedance_frequency method works correctly", {
   ncvar_put(nc, "country", mask_data)
   nc_close(nc)
   
-  wind <- WindComponent$new(u10_path, v10_path, mask_path, reference_period)
+  wind <- WindComponent$new(u10_path, v10_path, mask_path)
   wind_exceedance_frequency <- wind$wind_exceedance_frequency(reference_period)
   
-  print(wind_exceedance_frequency)  # Impression pour le débogage
-  
   expect_is(wind_exceedance_frequency, "data.table")
-  expect_true(all(c("longitude", "latitude", "exceedance_frequency") %in% colnames(wind_exceedance_frequency)))
+  expect_true(all(c("year_month", "latitude", "longitude", "exceedance_frequency") %in% colnames(wind_exceedance_frequency)))
   
   unlink(c(u10_path, v10_path, mask_path))
 })
@@ -190,13 +183,11 @@ test_that("WindComponent: std_wind_exceedance_frequency method works correctly",
   ncvar_put(nc, "country", mask_data)
   nc_close(nc)
   
-  wind <- WindComponent$new(u10_path, v10_path, mask_path, reference_period)
+  wind <- WindComponent$new(u10_path, v10_path, mask_path)
   std_wind_exceedance_frequency <- wind$std_wind_exceedance_frequency(reference_period)
   
-  print(std_wind_exceedance_frequency)  # Impression pour le débogage
-  
   expect_is(std_wind_exceedance_frequency, "data.table")
-  expect_true(all(c("longitude", "latitude", "std_frequency") %in% colnames(std_wind_exceedance_frequency)))
+  expect_true(all(c("year_month", "latitude", "longitude", "std_frequency") %in% colnames(std_wind_exceedance_frequency)))
   
   unlink(c(u10_path, v10_path, mask_path))
 })
@@ -212,17 +203,36 @@ test_that("WindComponent: std_wind_exceedance_frequency method works against pre
     mask_path <- file.path(data_dir, paste0(test_case, '_mask.nc'))
     reference_anomalies_path <- file.path(data_dir, paste0(test_case, '_reference_anomalies.nc'))
     
-    reference_anomalies <- ncdf4::nc_open(reference_anomalies_path)
-    wind_component <- WindComponent$new(u10_path, v10_path, mask_path, reference_period_bis)
-    
+    wind_component <- WindComponent$new(u10_path, v10_path, mask_path)
     calculated_anomalies <- wind_component$std_wind_exceedance_frequency(reference_period_bis)
     
-    reference_variable_name <- "standardized_frequency"
-    reference_data <- ncdf4::ncvar_get(reference_anomalies, reference_variable_name)
-    ncdf4::nc_close(reference_anomalies)
+    reference_anomalies <- nc_open(reference_anomalies_path)
+    reference_variable_name <- "__xarray_dataarray_variable__"
+    reference_data <- ncvar_get(reference_anomalies, reference_variable_name)
+    nc_close(reference_anomalies)
+    calculated_anomalies_avg <- calculated_anomalies[, .(mean_std_frequency = mean(std_frequency, na.rm = TRUE)), by = year_month]
     
-    expect_equal(calculated_anomalies$std_frequency, reference_data)
+    compare_vectors_with_nan <- function(vec1, vec2) {
+      if (length(vec1) != length(vec2)) {
+        return(FALSE)
+      }
+      for (i in seq_along(vec1)) {
+        if (is.nan(vec1[i]) && is.nan(vec2[i])) {
+          next
+        }
+        if (!all.equal(vec1[i], vec2[i], tolerance = 1e-6)) {
+          return(FALSE)
+        }
+      }
+      return(TRUE)
+    }
+    
+    if (compare_vectors_with_nan(calculated_anomalies_avg$mean_std_frequency, reference_data)) {
+      print("Passed")
+    } else {
+      print("Failed")
+    }
+    
+    expect_true(compare_vectors_with_nan(calculated_anomalies_avg$mean_std_frequency, reference_data))
   }
 })
-
-
