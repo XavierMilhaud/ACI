2df6c0e06b27e692e4e3464bc60170675839d04f
XavierMilhaud <xavier.milhaud.research@gmail.com>
2024-10-25 12:36:30 +0200

    Add PSMSL data

--- R/ActuarialClimateIndex.R
+++ R/ActuarialClimateIndex.R
@@ -1,4 +1,3 @@
-library(data.table)
 
 #' ActuarialClimateIndex Class
 #'
---
+++ R/psmsl.R
@@ -0,0 +1,19 @@
+#' PSMSL datasets on Sea Levels
+#'
+#' An example of datasets extracted from website giving...
+#'
+#' @format ## `psmsl`
+#' A data table (and thus also data frame) with 1,585 rows and 9 columns:
+#' \describe{
+#'   \item{Station Name}{(Character) The station name}
+#'   \item{ID}{(Integer) The raw id}
+#'   \item{Lat.}{(Numeric) The latitude of the station}
+#'   \item{Lon.}{(Numeric) The longitude of the station}
+#'   \item{GLOSS ID}{(Integer) The id in the gloassary}
+#'   \item{Country}{(Character) The country of the station}
+#'   \item{Date}{(Character) The date of measurement}
+#'   \item{Coastline}{(Character) The code of the coastline}
+#'   \item{Station}{(Character) The station id}
+#' }
+#' @source <https://psmsl.org/data/obtaining/>
+"psmsl"
